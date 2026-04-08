ae3e47a68b52029ea67ccb17c548cb26ba569a6f
keerthi1615 <2400030494@kluniversity.in>
2026-04-08 13:16:25 +0530

    added JPA repository scan

--- src/main/java/com/homevalue/backend/repository/PropertyRepository.java
+++ src/main/java/com/homevalue/backend/repository/PropertyRepository.java
@@ -1,7 +1,9 @@
 package com.homevalue.backend.repository;
 
-import com.homevalue.backend.model.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.homevalue.backend.model.Property;
 
+@Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
 }
