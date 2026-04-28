f0af054184ef6f83a6908829bccb78caf5673cd0
keerthi1615 <2400030494@kluniversity.in>
2026-04-28 14:31:25 +0530

    Initial backend commit

--- src/main/java/com/homevalue/backend/repository/RecommendationRepository.java
+++ src/main/java/com/homevalue/backend/repository/RecommendationRepository.java
@@ -1,8 +1,8 @@
 package com.homevalue.backend.repository;
 
+import com.homevalue.backend.model.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.homevalue.backend.model.Recommendation;
 
 @Repository
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
